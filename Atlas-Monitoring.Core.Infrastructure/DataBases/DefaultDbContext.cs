ad9bdb6c93196cae2f05d0eaebf9c71b5349cfc4
Paulin <buland2001@gmail.com>
2024-07-06 00:39:31 +0200

    Disable cascade delete

--- Atlas-Monitoring.Core.Infrastructure/DataBases/DefaultDbContext.cs
+++ Atlas-Monitoring.Core.Infrastructure/DataBases/DefaultDbContext.cs
@@ -26,6 +26,16 @@ namespace Atlas_Monitoring.Core.Infrastructure.DataBases
             modelBuilder.ApplyConfiguration(new EntityConfiguration.DeviceEntityTypeConfigurations());
             modelBuilder.ApplyConfiguration(new EntityConfiguration.DeviceHistoryEntityTypeConfigurations());
             modelBuilder.ApplyConfiguration(new EntityConfiguration.DeviceTypeEntityTypeConfigurations());
+
+            //Disable Delete cascade
+            var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+            foreach (var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+            }
         }
 
         #endregion
