126a73af00630b53a0e3c2a38488943bfd9f61b5
Paulin <buland2001@gmail.com>
2024-11-22 15:07:16 +0100

    Add migration

---
+++ Atlas-Monitoring.Core.Infrastructure/Migrations/20241122140505_AutomateReportAndLog.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas_Monitoring.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutomateReportAndLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AutomateLog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    AutomateId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Comment = table.Column<string>(type: "varchar(240)", maxLength: 240, nullable: false, defaultValue: "")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LogLevel = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutomateLog", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AutomateReport",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    EntityId = table.Column<Guid>(type: "char(36)", nullable: true, collation: "ascii_general_ci"),
+                    AppName = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false, defaultValue: "")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    GlobalMessage = table.Column<string>(type: "varchar(240)", maxLength: 240, nullable: false, defaultValue: "")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DurationInSeconds = table.Column<double>(type: "double", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutomateReport", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AutomateReport_Entity_EntityId",
+                        column: x => x.EntityId,
+                        principalTable: "Entity",
+                        principalColumn: "EntityId");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AutomateReport_EntityId",
+                table: "AutomateReport",
+                column: "EntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AutomateLog");
+
+            migrationBuilder.DropTable(
+                name: "AutomateReport");
+        }
+    }
+}
--- Atlas-Monitoring.Core.Models/Database/AutomateReport.cs
+++ Atlas-Monitoring.Core.Models/Database/AutomateReport.cs
@@ -5,7 +5,7 @@ namespace Atlas_Monitoring.Core.Models.Database
     public class AutomateReport
     {
         public Guid Id { get; set; }
-        public Entity? EntityId { get; set; }
+        public Entity? Entity { get; set; }
         public string AppName { get; set; } = string.Empty;
         public AutomateStatus Status { get; set; }
         public string GlobalMessage { get; set; } = string.Empty;
