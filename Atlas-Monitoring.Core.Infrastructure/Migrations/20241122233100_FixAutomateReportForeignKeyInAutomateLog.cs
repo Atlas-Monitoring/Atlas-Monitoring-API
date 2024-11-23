c89f40cf59cede0360a1ad590186ce90190f5227
Paulin <buland2001@gmail.com>
2024-11-23 01:44:00 +0100

    Correction of Migration

---
+++ Atlas-Monitoring.Core.Infrastructure/Migrations/20241122233100_FixAutomateReportForeignKeyInAutomateLog.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas_Monitoring.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAutomateReportForeignKeyInAutomateLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AutomateId",
+                table: "AutomateLog",
+                newName: "AutomateReportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AutomateLog_AutomateReportId",
+                table: "AutomateLog",
+                column: "AutomateReportId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AutomateLog_AutomateReport_AutomateReportId",
+                table: "AutomateLog",
+                column: "AutomateReportId",
+                principalTable: "AutomateReport",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AutomateLog_AutomateReport_AutomateReportId",
+                table: "AutomateLog");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AutomateLog_AutomateReportId",
+                table: "AutomateLog");
+
+            migrationBuilder.RenameColumn(
+                name: "AutomateReportId",
+                table: "AutomateLog",
+                newName: "AutomateId");
+        }
+    }
+}
